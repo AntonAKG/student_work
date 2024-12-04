bbfc4196745aa0ab3e0769f638c90ac38ca1af7a
Anton <106998036+AntonAKG@users.noreply.github.com>
2024-12-04 22:43:10 +0200

    add new page

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import HomeView
+from .views import HomeView, ContactView, AboutView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='index'),
+    path('contact/', HomeView.as_view(), name='contact'),
+    path('about/', HomeView.as_view(), name='about'),
 
 ]
--- main/views.py
+++ main/views.py
@@ -3,3 +3,9 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'main/home.html'
+
+class ContactView(TemplateView):
+    template_name = 'main/contact.html'
+
+class AboutView(TemplateView):
+    template_name = 'main/about.html'
